d4ac2d58c6edace1e8fb345ec22245a4992e4636
Daijon Bourne <Daijonbourne1005@gmail.com>
2024-04-06 04:39:00 -0700

    assisnment

---
+++ models/meet_greet.js
@@ -0,0 +1,46 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Meet_greet extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Meet_greet.init({
+    event_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    band_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    meet_start_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    meet_end_time: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    meet_greet_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+  }, {
+    sequelize,
+    modelName: 'Meet_greet',
+    tableName: 'Meet_greet',
+    timestamps: false
+  });
+  return Meet_greet;
+};
